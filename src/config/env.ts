daf82b3c3001948a3914e29ec5e472293bec5a5e
Muhammed Sinan <muhammedsinan7749@gmail.com>
2024-12-15 20:44:35 +0530

    routes added

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,10 @@ dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV;
 const MONGO_URI = process.env.MONGO_URI!;
+const CLIENT_URL = process.env.CLIENT_URL!;
 
 export {
     NODE_ENV,
-    MONGO_URI
+    MONGO_URI,
+    CLIENT_URL
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
+import cors from 'cors';
 import express from 'express';
 import connectDb from './config/connectDb';
+import authRoutes from './presentation/routers/authRoutes';
+import urlRoutes from './presentation/routers/urlRoutes';
+import { CLIENT_URL } from './config/env';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "Hello there" });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: CLIENT_URL,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
+app.use("/api", authRoutes);
+app.use("/api", urlRoutes);
 
 app.listen(8000, () => {
     connectDb();
