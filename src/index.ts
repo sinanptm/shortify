f3fef0596b0d0cfb1176d82317ce451ffa785f72
Muhammed Sinan <muhammedsinan7749@gmail.com>
2024-12-15 17:39:43 +0530

    Server setup completed

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"Hello there"});
+})
+
+app.listen(8000, () => {
+    console.log("Server start running");
+});
