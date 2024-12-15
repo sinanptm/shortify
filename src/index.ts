674dd0667d393137fac0e45c378c18ac23b75e92
Muhammed Sinan <muhammedsinan7749@gmail.com>
2024-12-15 17:42:59 +0530

    nodemon config addeds

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message:"Hello there"});
-})
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello there" });
+});
 
 app.listen(8000, () => {
     console.log("Server start running");
