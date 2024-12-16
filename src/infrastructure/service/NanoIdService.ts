6c8d8cee8715dd3dbe81a787f1e433ffd5a579c8
Muhammed Sinan <muhammedsinan7749@gmail.com>
2024-12-16 20:41:52 +0530

    nanoid import changed to dynamic

--- src/infrastructure/service/NanoIdService.ts
+++ src/infrastructure/service/NanoIdService.ts
@@ -1,8 +1,26 @@
-const { nanoid } = require("nanoid");
 import INanoIdService from "@/domain/interface/services/INanoIdService";
 
 export default class NanoIdService implements INanoIdService {
+    private nanoid!: (length: number) => string;
+
+    constructor() {
+        this.loadNanoId();
+    }
+
+    private async loadNanoId() {
+        try {
+            const { nanoid } = await import('nanoid');
+            this.nanoid = nanoid; 
+        } catch (error) {
+            console.error('Error loading nanoid:', error);
+            this.nanoid = () => ''; 
+        }
+    }
+
     generateId(length: number): string {
-        return nanoid(length);
+        if (!this.nanoid) {
+            throw new Error('Nanoid service not initialized');
+        }
+        return this.nanoid(length);
     }
 }
